6d9282ce092b1dfc8080b7b99973bb4739471798
rex <rex@rex-he.net>
2021-10-05 23:59:22 +1100

    update

--- keyboards/kbdfans/kbd75/keymaps/proheng/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/proheng/keymap.c
@@ -4,11 +4,13 @@
 const rgblight_segment_t PROGMEM my_layer0_layer[] = RGBLIGHT_LAYER_SEGMENTS( {1, 16, HSV_CORAL} );
 const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS( {1, 16, HSV_GOLDENROD} );
 const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS( {1, 16, HSV_ORANGE} );
+const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS( {1, 16, HSV_ORANGE} );
 
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     my_layer0_layer,    
     my_layer1_layer,
-    my_layer2_layer
+    my_layer2_layer,
+    my_layer3_layer
 );
 
 void keyboard_post_init_user(void) {
@@ -23,6 +25,7 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
 layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_layer_state(1, layer_state_cmp(state, 1));
     rgblight_set_layer_state(2, layer_state_cmp(state, 2));
+    rgblight_set_layer_state(3, layer_state_cmp(state, 3));
     return state;
 }
 
@@ -35,24 +38,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP, 
             KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGDN, 
             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END, 
-            KC_LCTL, KC_LALT, KC_LGUI, LT(2,KC_SPC),     VIM_ON  ,KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+            KC_LCTL, KC_LALT, KC_LGUI, LT(2,KC_SPC),     TG(2)  ,KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
         ),
     // Windows Layuout
 	[1] = LAYOUT_ansi_1u(
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,   _______, _______, 
+            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, 
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-            _______, _______, KC_LCTL, _______, _______, _______, _______, _______, _______, _______
+            _______, _______, KC_LCTL, LT(3,KC_SPC),     TG(3),   _______, _______, _______, _______, _______
         ),
-    // Share Layout
+    // MAC VIM Layout
 	[2] = LAYOUT_ansi_1u(
-            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   RESET,   KC_NO,   KC_NO,   
+            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______, KC_NO,   KC_NO,   
             KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   
             KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGUP, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   
             KC_NO,   KC_NO,   KC_NO,   KC_PGDN, KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   
             KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   
-            KC_NO,   KC_NO,   KC_NO,   KC_NO,   VIM_OFF, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
-            )
+            KC_NO,   KC_NO,   KC_NO,   KC_NO,            _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
+        ),
+    // Windows VIM Layuout
+	[3] = LAYOUT_ansi_1u(
+            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______, KC_NO,   KC_NO,   
+            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   
+            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGUP, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   
+            KC_NO,   KC_NO,   KC_NO,   KC_PGDN, KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   
+            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   
+            KC_NO,   KC_NO,   KC_NO,   KC_NO,            _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
+        )
 };
--- keyboards/kbdfans/kbd75/keymaps/proheng/macro.c
+++ keyboards/kbdfans/kbd75/keymaps/proheng/macro.c
@@ -1,25 +1,25 @@
 #include QMK_KEYBOARD_H
 
 
-enum custom_keycodes {
-    VIM_ON = SAFE_RANGE,
-    VIM_OFF
-};
+/* enum custom_keycodes { */
+/*     VIM_ON = SAFE_RANGE, */
+/*     VIM_OFF */
+/* }; */
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case VIM_ON:
-            if (record->event.pressed) {
-                layer_on(2);
-            } 
-            break;
-        case VIM_OFF:
-            if (record->event.pressed) {
-                layer_off(2);
-            } 
-            break;
-    }
+/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
+/*     switch (keycode) { */
+        /* case VIM_ON: */
+        /*     if (record->event.pressed) { */
+        /*         layer_on(2); */
+        /*     } */ 
+        /*     break; */
+        /* case VIM_OFF: */
+        /*     if (record->event.pressed) { */
+        /*         layer_off(2); */
+        /*     } */ 
+        /*     break; */
+    /* } */
 
-    return true;
-};
+    /* return true; */
+/* }; */
 
