f55e6834e5150ccacdf596c5c02919d212dc5392
rex <rex@rex-he.net>
2022-05-08 03:32:11 +1000

    update

--- keyboards/lily58/keymaps/proheng/keymap.c
+++ keyboards/lily58/keymaps/proheng/keymap.c
@@ -146,11 +146,6 @@ bool oled_task_user(void) {
   if (is_keyboard_master()) {
     // If you want to change the display of OLED, you need to change here
     oled_write_ln(read_layer_state(), false);
-    oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
-    oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
-    oled_write_ln(read_host_led_state(), false);
-    oled_write_ln(read_timelog(), false);
   } else {
     oled_write(read_logo(), false);
   }
