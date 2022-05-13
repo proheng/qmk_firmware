37090bf541762b5dfdb7b08772f2b3f9a1e553e4
rex <rex@rex-he.net>
2022-05-13 23:50:13 +1000

    update

--- keyboards/lily58/keymaps/proheng/keymap.c
+++ keyboards/lily58/keymaps/proheng/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   KC_TAB,   LCTL_T(KC_Q),   LALT_T(KC_W),   LGUI_T(KC_E),   LSFT_T(KC_R),    KC_T,              KC_Y,    RSFT_T(KC_U),  RGUI_T(KC_I), RALT_T(KC_O), RCTL_T(KC_P), KC_EQL,
   TD(TD_CAP),  KC_A,   KC_S,    KC_D,    KC_F, KC_G,                     KC_H,    KC_J,    KC_K, KC_L, KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LEFT_KNOB,            RIGHT_KNOB,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+  KC_LSFT,  LT(0,KC_Z),  LT(0,KC_X),    LT(0,KC_C),    LT(0,KC_V),    KC_B, LEFT_KNOB,            RIGHT_KNOB,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
                    KC_LGUI, KC_LALT, LCTL_T(KC_SPC),C_S_T(KC_ENT),         KC_ENT, LT(_VIM, KC_BSPC), TD(TD_CAD), KC_RGUI   
 ),
 /* _VIM
@@ -195,6 +195,29 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             oled_clear();
           }
           return true;
+        case LT(0,KC_X):
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(C(KC_X)); // Intercept hold function to send Ctrl-X
+                return true;             // Return true for normal processing of tap keycode
+            }
+        case LT(0,KC_C):
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(RCTL(KC_INS)); // Intercept hold function to send Ctrl-C
+                return true;
+            }
+            return true;             // Return true for normal processing of tap keycode
+        case LT(0,KC_V):
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(RSFT(KC_INS)); // Intercept hold function to send Ctrl-V
+                return true;
+            }
+            return true;             // Return true for normal processing of tap keycode
+        case LT(0,KC_Z):
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(C(KC_Z)); // Intercept hold function to send Ctrl-V
+                return true;
+            }
+            return true;             // Return true for normal processing of tap keycode
         default:
           return true;
     }
