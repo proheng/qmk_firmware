b5da8e32ed95784ef30eb6d5e841313ba65bc6fd
Rex He <rex@zhehengs-mini.lan>
2022-06-22 20:16:14 +1000

    small adjustment

--- keyboards/dztech/dz60rgb_ansi/keymaps/proheng/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/proheng/keymap.c
@@ -22,7 +22,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[MAC_QWERTY] = LAYOUT_60_ansi(
         KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,    KC_5,   KC_6,    KC_7,    KC_8,   KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  
-        KC_TAB,  LCTL_T(KC_Q),    LALT_T(KC_W),    LGUI_T(KC_E),    LSFT_T(KC_R),    KC_T,    KC_Y,    RSFT_T(KC_U),    RGUI_T(KC_I),    RALT_T(KC_O),    RCTL_T(KC_P),    KC_LBRC, KC_RBRC, KC_BSLS, 
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, 
         TD(TD_CAP),       KC_A,     KC_S,     KC_D,     KC_F,     KC_G,    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  
         KC_LSFT, KC_Z,    LT(0,KC_X),    LT(0,KC_C),   LT(0,KC_V),    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  
         KC_LCTL, KC_LALT, KC_LGUI,    LT(MAC_VIM, KC_SPC),       TD(TD_FN),     KC_RGUI, KC_RALT, TD(TD_RCTL)
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Windows Layuout
 	[WIN_QWERTY] = LAYOUT_60_ansi(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-        _______, C_S_T(KC_Q), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
         TD(TD_CAP),       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
         _______, _______, _______,          LT(WIN_VIM, KC_SPC),       _______, _______, _______, _______
